4fffe3d16c05cc98e4794b5ac59492fea4d4e853
Andreas Dieckmann <andreas@badmonkeys.net>
2024-08-08 22:40:33 +0200

    fixed #66

--- nodes/3.x/python/Journal.Exceptions.py
+++ nodes/3.x/python/Journal.Exceptions.py
@@ -22,6 +22,7 @@ def journalGetExceptions(journal):
 		excobjs = []
 		for line in exclines:
 			newexc = RecordedException(line)
+			addnewexc = True
 			if line.Type == "JournalTimeStamp": 
 				lp1 = line.Description.split("ApplicationException is being thrown on behalf of the function <",1)
 				if len(lp1) > 1:
@@ -34,20 +35,29 @@ def journalGetExceptions(journal):
 					elif line.Description.startswith("ExceptionCode"): newexc.Type = "Fatal Error"
 					newexc.Message = line.Description
 			elif line.Type == "JournalAPIMessage": 
-				lp1 = line.MessageText.split(" exception(",1)
-				newexc.Type = lp1[0]
-				if len(lp1) > 1: 
-					lp2 = lp1[1].split(") was thrown from a handler of ",1)
-					newexc.Message = lp2[0]
-					if len(lp2) > 1: 
-						lp3 = lp2[1].split("event. The API event handler was registered by application ",1)
-						newexc.Event = lp3[0]
-						if len(lp3) > 1: 
-							lp4 = lp3[1].split(" (",1)
-							newexc.AppName = lp4[0]
-							if len(lp4) > 1:
-								lp5 = lp4[1].split(")",1)
-								newexc.AppGUID = lp5[0]
+				if line.MessageText.startswith("An external service execution throws"):
+					addnewexc = False
+					lp1 = line.MessageText.split(":")
+					excobjs[-1].Message = excobjs[-1].Message + " " + lp1[0]
+					if len(lp1) > 1:
+						lp2 = lp1[1].split("; ")
+						excobjs[-1].AppName = lp2[0].replace(" Name(","").replace(")","")
+						if len(lp2) > 3: excobjs[-1].AppGUID = lp2[2] + ", " + lp2[3]
+				else:
+					lp1 = line.MessageText.split(" exception(",1)
+					newexc.Type = lp1[0]
+					if len(lp1) > 1: 
+						lp2 = lp1[1].split(") was thrown from a handler of ",1)
+						newexc.Message = lp2[0]
+						if len(lp2) > 1: 
+							lp3 = lp2[1].split("event. The API event handler was registered by application ",1)
+							newexc.Event = lp3[0]
+							if len(lp3) > 1: 
+								lp4 = lp3[1].split(" (",1)
+								newexc.AppName = lp4[0]
+								if len(lp4) > 1:
+									lp5 = lp4[1].split(")",1)
+									newexc.AppGUID = lp5[0]
 			elif line.Type == "JournalComment":
 				lp1 = line.RawText.split("Exception caught from managed method ",1)
 				if len(lp1) > 1:
@@ -59,13 +69,29 @@ def journalGetExceptions(journal):
 						if len(lp3) > 1: newexc.Message = lp3[1].strip()[:-1]
 				else: 
 					lp1 = line.RawText.split("External Command Registration Exception: ",1)
+					lp2 = line.RawText.split(":< ")
 					if len(lp1) > 1:
 						newexc.Type = "External Command Registration Exception"
 						newexc.Message = lp1[1]
-					elif "OpenStream FileException" in line.RawText: newexc.Type = "OpenStream FileException"
+					elif "OpenStream FileException" in line.RawText: 
+						newexc.Type = "OpenStream FileException"
+						newexc.Message = line.RawText.split("OpenStream FileException",1)[1].strip()
+					elif "exceptionsInOnOpenDocument" in line.RawText:
+						addnewexc = False
+						lp3 = line.RawText.split("<<")
+						if len(lp3) > 1: excobjs[-1].Message = excobjs[-1].Message + " " + lp3[1]
+					elif len(lp2) > 1: 
+						if "ExceptionProxy:" in lp2[1]:
+							lp3 = lp2[1].split(": ",1)
+							newexc.Type = lp3[0]
+							if len(lp3) > 1:
+								lp4 = lp3[1].split(" at",1)
+								newexc.Message = lp4[0]
+								if len(lp4) > 1: newexc.StackTrace = "at" + lp4[1]
+						else: newexc.Message = lp2[1]
 					else: newexc.Message = line.RawText
 			else: newexc.Message = line.RawText
-			excobjs.append(newexc)
+			if addnewexc: excobjs.append(newexc)
 		return excobjs
 	else: return []
 
