80deed1e53134929bd7daaecec927c2cdcaa56e0
Andreas Dieckmann <adieckmann@gmp.de>
2024-10-14 18:50:42 +0200

    fixed #64

--- nodes/2.x/python/Journal.StripComments.py
+++ nodes/2.x/python/Journal.StripComments.py
@@ -1,7 +1,7 @@
 import clr
 
 def process_input(func, input1, input2):
-	if isinstance(input, list): return [func(x, input2) for x in input1]
+	if isinstance(input1, list): return [func(x, input2) for x in input1]
 	else: return func(input1, input2)
 	
 def journalStripComments(journal, preserveTimestamps):
